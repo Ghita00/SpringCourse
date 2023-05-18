68833b20b3e251259b56f5b87b450d0ed6757cb0
G.Basile <G.basile@almaviva.it>
2023-05-18 13:03:58 +0200

    first commit

---
+++ src/test/java/com/apiRestSpring/demo/ApiRestSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apiRestSpring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
