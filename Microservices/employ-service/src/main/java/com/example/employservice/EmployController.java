ad5e2e42609b4bf4ed0607ac668a3650a2c4c3ca
G.Basile <G.basile@almaviva.it>
2023-05-23 09:53:46 +0200

    call another service

--- Microservices/employ-service/src/main/java/com/example/employservice/EmployController.java
+++ Microservices/employ-service/src/main/java/com/example/employservice/EmployController.java
@@ -1,14 +1,22 @@
 package com.example.employservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class EmployController {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/dipendente")
+
     public String getEmploy(){
-        return "Giorgio Basile";
+        //get call to URL and map the resul in String type
+        String indirizzo = restTemplate.getForObject("http://localhost:8080/indirizzo", String.class);
+        return "Giorgio Basile " + indirizzo;
     }
 
 }
